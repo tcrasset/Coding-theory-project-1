6dfd02ffad0deac32bc12f98fa1e70bee0283390
“Antoine <antoine.louis02@outlook.fr>
2019-03-10 21:34:19 +0100

    Correct mutual information

--- entropy.py
+++ entropy.py
@@ -1,24 +1,22 @@
 import numpy
 import math
 
-# joint distribution[y][x]
-joint_distribution = numpy.array(
-    [[1/8, 1/16, 1/16, 1/4],
-    [1/16, 1/8, 1/16, 0],
-    [1/32, 1/32, 1/16, 0],
-    [1/32, 1/32, 1/16, 0]]
-)
-
-print(joint_distribution[0])
-
 
 def entropy(probability_distribution):
+    """
+    Computes H(X), the entropy of a random variable X, given its probability
+    distribution.
+    """
     entropy = 0
     for prob in probability_distribution:
         entropy += prob * math.log(prob ,2)
     return -entropy
 
 def joint_entropy(joint_distribution):
+    """
+    Computes H(X,Y), the joint entropy of two discrete random variable X
+    and Y, given their joint probability distribution.
+    """
     len_y = joint_distribution.shape[0]
     len_x = joint_distribution.shape[1]
     entropy = 0
@@ -30,15 +28,14 @@ def joint_entropy(joint_distribution):
 
 def conditional_entropy(joint_distribution):
     """
-    Computes the conditional entropy H(x|y), using
-    an array where y are the rows and x are the colums,
-    like so : joint distribution[y][x]
+    Computes H(X|Y), the conditional entropy of two discrete random variable X
+    and Y, given their joint probability distribution.
     """
     len_y = joint_distribution.shape[0]
     len_x = joint_distribution.shape[1]
     entropy = 0
     for y in range(len_y):
-        prob_y = sum(joint_distribution[y])
+        prob_y = sum(joint_distribution[y,:])
         for x in range(len_x):
             entropy += joint_distribution[y,x] * math.log(joint_distribution[y, x]/prob_y, 2)
     return -entropy
@@ -46,13 +43,27 @@ def conditional_entropy(joint_distribution):
 
 def mutual_information(joint_distribution):
     """
-    Computes the mutual information I(x|y), using
-    a numpy array where y are the rows and x are the colums,
-    like so : joint distribution[y][x]
+    Computes I(X;Y), the mutual information between two discrete random variable X
+    and Y, given their joint probability distribution.
     """
+    len_y = joint_distribution.shape[0]
     len_x = joint_distribution.shape[1]
-    prob_x= [sum(joint_distribution[:,i]) for i in range(len_x)]
+    entropy = 0
+    for y in range(len_y):
+        prob_y = sum(joint_distribution[y,:])
+        for x in range(len_x):
+            prob_x = sum(joint_distribution[:,x])
+            entropy += joint_distribution[y,x] * math.log(joint_distribution[y, x]/(prob_x * prob_y), 2)
+    return -entropy
 
-    return entropy(prob_x) - conditional_entropy(joint_distribution)
 
+
+if __name__ == "__main__":
+    # Joint distribution[y][x]
+    joint_distribution = numpy.array(
+        [[1/8, 1/16, 1/16, 1/4],
+        [1/16, 1/8, 1/16, 0],
+        [1/32, 1/32, 1/16, 0],
+        [1/32, 1/32, 1/16, 0]]
+    )
 
